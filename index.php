ba6a70fd99a9a10acbf1e46833c8c40b2d2f13b4
Sichina <John.Sichina@ad.local>
2015-11-13 14:32:51 -0500

    index.php uses src relative path

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php 
-	readfile("/src/index.html");
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Algebraic</title>
+	<link rel="stylesheet" href="src/style.css">
+</head>
+<body>
+
+	<form oninput="compute(equation, formattedInput, result); return false;">
+		<input type="text" name="equation" autocomplete="off" autofocus/>
+		<output name="formattedInput"></output>
+		<output name="result"></output>
+	</form>
+
+	<script src="src/scripts/polyfill.js"></script>
+	<script src="src/scripts/models/Scope.js"></script>  
+	<script src="src/scripts/models/ValueOperatorNodes.js"></script>  
+	<script src="src/scripts/compute.js"></script>
+
+</body>
+</html>

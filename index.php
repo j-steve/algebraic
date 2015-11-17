f2b26ee362dfce81ee5fd9c818dff5989deabb48
Sichina <John.Sichina@ad.local>
2015-11-17 13:36:00 -0500

    index.php as redirect

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-	var $index_html = readfile('src/index.html');
-	$index_html = str_replace($index_html, 'href="', 'href="src/');
-	$index_html = str_replace($index_html, 'src="', 'src="src/');
-	echo $index_html;
+
+header("Location: http://algebraic.herokuapp.com/src/");
+die();
+
 ?>

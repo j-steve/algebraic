d51ef2df79265479cf6bfd896da7ef2caecbd193
John.Sichina <John.Sichina@F1710-JXGNJ12-L.ad.local>
2015-11-20 12:34:42 -0500

    configure Grunt to build to www-built directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,47 @@
  * and open the template in the editor.
  */
 module.exports = function (grunt) {
-    // Project configuration.
-    grunt.initConfig({
-    });
+
+	grunt.initConfig({
+		jshint: {
+			files: ['Gruntfile.js', 'www/scripts/**/*.js'],
+			options: {
+			}
+		},
+		watch: {
+			files: ['<%= jshint.files %>'],
+			tasks: ['build']
+		},
+		copy: {
+			build: {
+				cwd: 'www',
+				src: ['**'],
+				dest: 'www-built',
+				expand: true
+			}
+		},
+		clean: {
+			build: {
+				src: 'www-built'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	
+	grunt.registerTask(
+	  'default', 
+	  'Watches the project for changes, automatically builds them and runs a server.', 
+	  [ 'build', 'watch' ]
+	);
+	
+	grunt.registerTask(
+	  'build', 
+	  'Compiles all of the assets and copies the files to the build directory.', 
+	  ['jshint', 'clean', 'copy' ]
+	);
+
 };

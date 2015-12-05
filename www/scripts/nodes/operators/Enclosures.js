6f6bb220630f0bc6dc811677613be7bf3ad8e10f
Steve <Steve5805@gmail.com>
2015-12-04 21:05:27 -0500

    add simplification to parenthesis node

--- www/scripts/nodes/operators/Enclosures.js
+++ www/scripts/nodes/operators/Enclosures.js
@@ -16,11 +16,17 @@ function EnclosureNode(openSymbol, closeSymbol) {
 	self.openSymbol = openSymbol || '';
 	self.closeSymbol = closeSymbol || ''; 
 	
-	EnclosureNode.$super(self, self.openSymbol + self.closeSymbol);
+	var $super = EnclosureNode.$super(self, self.openSymbol + self.closeSymbol);
 	
 	self.printVals.before += self.openSymbol;
 	
 	self.printVals.after = self.closeSymbol + self.printVals.after;
+	
+	
+	this.simplify = function() { 
+		$super.simplify();
+		if (self.leftNode instanceof LeafNode) {return self.leftNode;}
+	};
 } 
 
 Object.extend(EnclosureNode, ParenthesisNode);
